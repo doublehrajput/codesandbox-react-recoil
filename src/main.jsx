1fc9f6b4d3382c8e1292421b55222afb1deeb402
Carlos <desarrollo.sistemas@corporacionazul.com.mx>
2023-02-20 09:50:07 -0600

    CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import { FirstApp } from './FirstApp';
-import { HelloWorldApp } from './HelloWorldApp';
+// import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+// import { HelloWorldApp } from './HelloWorldApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* <HelloWorldApp/> */}
-        <FirstApp />
+        <CounterApp value={ 1998 }/>
     </React.StrictMode>
 )
