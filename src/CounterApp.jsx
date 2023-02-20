21fd5a3fc497a517ae96f407049cd8efd0c17a14
Carlos Villaseñor Quintero <charlyqv98@gmail.com>
2023-02-20 10:16:23 -0600

    Evento Click

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -2,10 +2,21 @@ import PropTypes from 'prop-types';
 
 export const CounterApp = ( { value } ) => {
   
+  const handleAdd = () => { 
+    // console.log(event)
+    console.log('+1');
+    value = 1000;
+    console.log(value);
+  } 
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{ value }</h2>
+      <h2> { value } </h2>
+
+      <button onClick={ handleAdd }>
+        +1
+      </button>
     </>
   )
 }
