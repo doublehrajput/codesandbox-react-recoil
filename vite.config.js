8c793f1b19028a5994dcb16d669bbf23346cb6a2
Harish rajput <doublehrajput@gmail.com>
2023-02-21 04:04:56 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
+ 
   resolve: {
     alias: {
       "@app": "/src",
