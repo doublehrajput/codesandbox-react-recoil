fd42371b140e92eb13b428663cd6fc0198e606de
Harish rajput <doublehrajput@gmail.com>
2023-02-21 09:27:23 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { RecoilRoot } from "recoil";
+import "@app/App.scss";
+
+const App = () => {
+  return (
+    <RecoilRoot>
+      <div>App</div>
+    </RecoilRoot>
+  );
+};
+
+App.defaultProps = {};
+App.propTypes = {};
+
+export default App;
